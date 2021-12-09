883fde04eaa1a25fda333425b520dfd1e356d85e
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-09 20:38:46 -0300

    requisito 7 concluido

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function blackSet() {
     let blackElement = document.getElementById('black');
     blackElement.classList.add('selected');
  };
+
+ let colorsArray = document.getElementsByClassName('color');
+
+function changeColor() {    
+    for (let i = 0; i < colorsArray.length; i += 1) {   
+    colorsArray[i].addEventListener('click', function(event){
+    let colorSelected = document.querySelector('.selected');   
+    colorSelected.className = 'color';
+    event.target.className = 'color selected';        
+    }  
+    )
+}
+};
+changeColor();
