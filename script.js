95568ff93a1e08ac12bb26c0d95b7c405560294e
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-09 21:59:13 -0300

    requisito 8 concluido

--- script.js
+++ script.js
@@ -10,12 +10,25 @@ function blackSet() {
 
 function changeColor() {    
     for (let i = 0; i < colorsArray.length; i += 1) {   
-    colorsArray[i].addEventListener('click', function(event){
-    let colorSelected = document.querySelector('.selected');   
-    colorSelected.className = 'color';
-    event.target.className = 'color selected';        
+        colorsArray[i].addEventListener('click', function(event){
+        let colorSelected = document.querySelector('.selected');   
+        colorSelected.className = 'color';
+        event.target.className = 'color selected';        
     }  
     )
 }
 };
 changeColor();
+let pixelCollection = document.getElementsByClassName('pixel');
+
+function colorPixel() {
+    for (let i = 0; i < pixelCollection.length; i += 1) {
+        pixelCollection[i].addEventListener('click', function(event) {
+            let colorSelected = document.querySelector('.selected').id;
+            let pixel = event.target;
+            pixel.style.backgroundColor = colorSelected;
+        })
+    }
+    
+};
+colorPixel();
