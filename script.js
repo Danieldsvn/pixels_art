678380284e84aa11e18783a556cb5550147a5a5a
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-10 13:54:14 -0300

    Refazendo requisitos obrigatorios. Tornando-os dinamicos. 1 ao 4 refeitos.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+//requisito 1
+
+let body = document.querySelector('body');
+let h1Title = document.createElement('h1');
+h1Title.id = 'title';
+h1Title.innerText = 'Paleta de Cores';
+body.appendChild(h1Title);
+
+//requisito 2
+
+// cria elemento div com id color-pallet
+let divColorPalett = document.createElement('div');
+divColorPalett.id = 'color-palett';
+body.appendChild(divColorPalett);
+// cria elementos div com class color
+for (let i = 1; i <= 4; i += 1) {
+    let colorSpan = document.createElement('span');
+    colorSpan.className = 'color';
+    colorSpan.style.border = 'solid black 1px';
+    divColorPalett.appendChild(colorSpan);
+
+}
+let colorsToChoose = document.getElementsByClassName('color');
+colorsToChoose[0].id = 'black';
+colorsToChoose[0].innerText = "Preto";
+colorsToChoose[0].style.backgroundColor = 'black';
+colorsToChoose[0].style.color = 'white';
+colorsToChoose[1].id = 'red';
+colorsToChoose[1].innerText = "Vermelho";
+colorsToChoose[1].style.backgroundColor = 'red';
+colorsToChoose[2].id = 'green';
+colorsToChoose[2].innerText = "Verde";
+colorsToChoose[2].style.backgroundColor = 'green';
+colorsToChoose[3].id = 'blue';
+colorsToChoose[3].innerText = "Azul";
+colorsToChoose[3].style.backgroundColor = 'blue';
 
 
-// Seleciona cor preta ao carregar a página
+//Requisito 3
 window.onload = blackSet();
 
 function blackSet() {
@@ -10,8 +46,32 @@ function blackSet() {
 
  let colorsArray = document.getElementsByClassName('color');
 
-// Prepara a seleção de cores para aplicar nos pixels.
-function changeColor() {    
+ // Requisito 4
+// Cria div pixel-board
+ let divPixelBoard = document.createElement('div');
+ divPixelBoard.id = 'pixel-board';
+ body.appendChild(divPixelBoard);
+
+ // Cria elementos pixels 5x5
+ function pixelsCreator() {
+    for (let i = 1; i <= 5; i += 1) {
+        let divPixelBoardLine = document.createElement('div');
+        divPixelBoardLine.className = 'pixel-board-line';
+        divPixelBoard.appendChild(divPixelBoardLine);
+        for (let j = 1; j <= 5; j += 1) {
+            let divPixel = document.createElement('div');
+            divPixel.className = 'pixel';
+            divPixelBoardLine.appendChild(divPixel);
+        }  
+    }
+ }
+ pixelsCreator();
+
+// requisito 5
+
+
+ // Prepara a seleção de cores para aplicar nos pixels.
+ function changeColor() {    
     for (let i = 0; i < colorsArray.length; i += 1) {   
         colorsArray[i].addEventListener('click', function(event){
         let colorSelected = document.querySelector('.selected');   
@@ -20,7 +80,7 @@ function changeColor() {
     }  
     )
 }
-};
+}; 
 
 // Aplica cores nos pixels por clique conforme cor selecionada.
 changeColor();
@@ -38,7 +98,7 @@ function colorPixel() {
 };
 colorPixel();
 
-//Cria botão que  limpa os pixels e o torna branco novamente.
+// Cria botão que  limpa os pixels e o torna branco novamente.
 
 let button = document.getElementById('clear-board');
 
@@ -49,3 +109,4 @@ button.addEventListener('click', function() {
     }
 });
 
+ 
